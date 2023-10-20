6970dbec28473e76f46c3379ff49fb8972dd3211
havehope <1976836@gmail.com>
2023-10-20 13:51:00 +0700

    pull code from origin dev

--- src/component/admin/Management.js
+++ src/component/admin/Management.js
@@ -253,11 +253,11 @@ export default function DataTable() {
                                                         onClose={handleClose}
                                                         aria-describedby="alert-dialog-slide-description"
                                                     >
-                                                        <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+                                                        <DialogTitle>{"Bạn có muốn kích hoạt cho tài khoản này?"}</DialogTitle>
                                                         <DialogContent>
                                                             <DialogContentText id="alert-dialog-slide-description">
-                                                                Let Google help apps determine location. This means sending anonymous
-                                                                location data to Google, even when no apps are running.
+                                                                Chủ sở hữu tài khoản sẽ được xác thực. Tên ca sĩ sẽ được 
+                                                                cho thêm vào danh sách.
                                                             </DialogContentText>
                                                         </DialogContent>
                                                         <DialogActions>
